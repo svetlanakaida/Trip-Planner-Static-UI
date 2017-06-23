2c786dcad8cfb8ff71b72b77e1e3085527956394
svetlanakaida <svetlanakaida@users.noreply.github.com>
2017-06-23 13:04:01 -0400

    start with layout.html

--- start/app.js
+++ start/app.js
@@ -5,16 +5,21 @@ const volleyball = require('volleyball');
 const bodyParser = require('body-parser');
 const nunjucks = require('nunjucks');
 const routes = require('./routes');
-
+const path = require('path');
 app.use(volleyball);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/bootstrap', express.static(path.join(__dirname, "node_modules/bootstrap/dist")));
+app.use('/jquery', express.static(path.join(__dirname, "node_modules/jquery/dist")));
+// app.use('/jquery', express.static(path.join(__dirname, "node_modules/jquery/dist")));
 app.use(express.static(__dirname + '/public'));
 
 app.set('view engine', 'html');
 app.engine('html', nunjucks.render);
 nunjucks.configure('views', { noCache: true });
+// app.use(express.static("node_modules"));
+
 
 app.use('/', routes);
 
